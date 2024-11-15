d3ecb3298068836889c5ba200f13377914a67d20
Aleks S <alesek23@bu.edu>
2024-11-15 14:33:40 -0500

    Add Files

---
+++ app/src/main/java/com/example/aleksandarsekulovskiefimsokolov_moviematcherfinalproject/MyAppNavigation.kt
@@ -0,0 +1,28 @@
+package com.example.aleksandarsekulovskiefimsokolov_moviematcherfinalproject
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.aleksandarsekulovskiefimsokolov_moviematcherfinalproject.pages.Homepage
+import com.example.aleksandarsekulovskiefimsokolov_moviematcherfinalproject.pages.LoginPage
+import com.example.aleksandarsekulovskiefimsokolov_moviematcherfinalproject.pages.SignupPage
+
+@Composable
+fun MyAppNavigation(modifier : Modifier = Modifier, authViewModel: AuthViewModel) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login", builder = {
+        composable("login") {
+            LoginPage(modifier, navController, authViewModel)
+        }
+        composable("signup") {
+            SignupPage(modifier, navController, authViewModel)
+        }
+        composable("home") {
+            Homepage(modifier, navController, authViewModel)
+        }
+    })
+
+}
---
+++ app/src/main/java/com/example/aleksandarsekulovskiefimsokolov_moviematcherfinalproject/pages/HomePage.kt
@@ -0,0 +1,11 @@
+package com.example.aleksandarsekulovskiefimsokolov_moviematcherfinalproject.pages
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.aleksandarsekulovskiefimsokolov_moviematcherfinalproject.AuthViewModel
+
+@Composable
+fun Homepage(modifier : Modifier = Modifier, navController: NavController, authViewModel: AuthViewModel) {
+
+}
